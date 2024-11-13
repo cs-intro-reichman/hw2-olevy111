9e7c609423c783a0623f777b03d3359d143a264e
Or Levy <olevy111@gmail.com>
2024-11-13 20:33:15 +0200

    Update Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,28 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String N =  args[0]; 
+                int rows = Integer.parseInt(args[1]);
+                String BN = N.toUpperCase();
+                String letter = "AEFHILMNORSX";
+                for (int i=0 ; i<BN.length(); i++)
+                {
+                        char c = BN.charAt(i);
+                        if (letter.indexOf(c)!= -1)
+                        {
+                        System.out.println("Give me an " + BN.charAt(i) + ": " + BN.charAt(i) + "!");
+                        }
+                        else
+                        {
+                        System.out.println("Give me a  " + BN.charAt(i) + ": " + BN.charAt(i) + "!");      
+                        }
+                }
+                System.out.println("What does it spell?");
+                for (int i=0 ; i<rows ; i++)
+                {
+                        System.out.println(BN + "!!!");
+                }
+                
+
         }
 }
