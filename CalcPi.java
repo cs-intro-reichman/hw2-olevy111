6b4fcc30a0fbc9d12bf456c5fc2b4f44ca01f9bf
Or Levy <olevy111@gmail.com>
2024-11-13 21:55:52 +0200

    Update CalcPi.java

--- CalcPi.java
+++ CalcPi.java
@@ -3,13 +3,13 @@ public class CalcPi {
 	public static void main(String [] args) { 
 	    int num = Integer.parseInt(args[0]);
 		double CalcPi = 0;
-		int n = 1;
+		int n = 0;
 		for (int i=0 ; i<num ; i++)
 		{
-			CalcPi = CalcPi + (1.0/n)-(1.0/(n+2.0));
-			n = n+4;
+			CalcPi = CalcPi + (1.0/((2*n)+1))-(1.0/((2*n)+3));
+			n = n+2;
 		}
 		System.out.println("pi according to Java: " + Math.PI);
-		System.out.println("pi, approximated:    " + CalcPi*4.0);
+		System.out.println("pi, approximated:     " + CalcPi*4.0);
 	}
 }
