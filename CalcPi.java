8f4f72998714bd53126c0679083e0a0dd2c37c83
Or Levy <olevy111@gmail.com>
2024-11-13 21:28:19 +0200

    Update CalcPi.java

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,16 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int num = Integer.parseInt(args[0]);
+		double CalcPi = 0;
+		int n = 1;
+		for (int i=0 ; i<num ; i++)
+		{
+			double sum = (1.0/n)-(1.0/(n+2.0));
+			CalcPi = CalcPi + sum;
+			n = n+4;
+		}
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated: " + CalcPi*4.0);
 	}
 }
